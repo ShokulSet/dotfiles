af0c7878cca620fe51d48375ba5156c86823c515
ShokulSet <104113257+ShokulSet@users.noreply.github.com>
2024-06-20 20:06:10 +0700

    Adding bunch of config files from my Sway desktop

---
+++ dot_config/powerline/version.py
@@ -0,0 +1,7 @@
+# vim:fileencoding=utf-8:noet
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+__version__ = "2.8.3"
+
+def get_version():
+	return __version__
